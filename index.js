ba614598789b7144e8855ebf6203c297526c63c7
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-12 23:42:06 -0500

    lol

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/webhook/', function (req, res) {
                 pg.connect(process.env.DATABASE_URL, function(err, client) {
                     if (err) throw err;
                     client
-                        .query("SELECT id FROM user_table where name ='" + username + "'")
+                        .query("SELECT id FROM user_table WHERE name = " + username)
                         .on('row', function(row) {
                             challenge_id = JSON.stringify(row);
                         });
