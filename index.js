d757011b369f5b440486f89395ff253dbb6a2d16
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-14 15:49:04 -0500

    comeon

--- index.js
+++ index.js
@@ -525,16 +525,24 @@ function makeMove(attacker_id, defender_id, health_defender, health_attacker, at
     }else{
         new_health_def = health_defender - attack_value;
         //update the duel
-        q = 'UPDATE duel_table SET (user_turn = \'' + defender_id + '\', health_sender = '+new_health_def+') WHERE duel_id = '+duel_id;
+        sendTextMessage(defender_id, "sdfsdfsd");
+        sendTextMessage(attacker_id, "sdfsdfsd");
+        q = 'UPDATE duel_table SET (user_turn = \'' + defender_id + '\', health_sender = '+new_health_def+', moves_in_duel = moves_in_duel + 1) WHERE duel_id = '+duel_id;
         if (attacker_is_sender) {
-            q = 'UPDATE duel_table SET (user_turn = \'' + defender_id + '\', health_recipient = '+new_health_def+') WHERE duel_id = '+duel_id;
+            q = 'UPDATE duel_table SET (user_turn = \'' + defender_id + '\', health_recipient = '+new_health_def+', moves_in_duel = moves_in_duel + 1) WHERE duel_id = '+duel_id;
         }
+        sendTextMessage(defender_id, "sdfsdfsd2");
+        sendTextMessage(attacker_id, "sdfsdfsd2");
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query(q, function(err, result) {
                 done();
+                sendTextMessage(defender_id, "sdfsdfsd3");
+                sendTextMessage(attacker_id, "sdfsdfsd3");
                 if (err || result.rows.length !== 1) {
                     sendTextMessage(attacker_id, "error (8)");
                 }else{
+                    sendTextMessage(defender_id, "sdfsdfsd4");
+                    sendTextMessage(attacker_id, "sdfsdfsd4");
                     sendTextMessage(defender_id, attacker_name+" hit you for "+attack_value+" hp!");
                     sendTextMessage(attacker_id, "You hit "+defender_name+" for "+attack_value+" hp!");
                     sendTextMessage(defender_id, attacker_name+": "+health_attacker+" ||| "+defender_name+": "+new_health_def);
