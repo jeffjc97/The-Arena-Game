90012dff5b0f3eddd7c286a19e4624efcfa6c9cd
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-14 17:47:46 -0500

    oops

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ function registerUser(s, username) {
     }
     else {
         sendTextMessage(s, username);
-        q_add_username = "INSERT INTO user_table(id, name) VALUES (\'" + s + "\', \'" + username + "\')";
+        q_add_username = "INSERT INTO user_table VALUES (\'" + s + "\', \'" + username + "\')";
         e = function(err) {
             sendTextMessage(s, JSON.stringify(err).substring(0,200));
             // if (err.detail.indexOf("already exists") > -1) {
