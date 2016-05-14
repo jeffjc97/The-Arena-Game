7bd5b78ef047a3275cfd7e2812300f42a8cedb82
Roy Falik <roy.falik@utexas.edu>
2016-05-13 20:54:26 -0500

    added timestamp to duel_table

--- default_inserts.sql
+++ default_inserts.sql
@@ -1,5 +1,6 @@
  ALTER TABLE challenge_table ADD CONSTRAINT uniqueChallenge UNIQUE (sender, recipient);
  ALTER TABLE challenge_table ALTER issued SET DEFAULT CURRENT_TIMESTAMP;
+  ALTER TABLE duel_table ALTER time_started SET DEFAULT CURRENT_TIMESTAMP;
 
  insert into user_table(id, name, in_duel) VALUES ('10206557582650156', 'jeff', 0);
  insert into user_table(id, name, in_duel) VALUES ('10205320360242528', 'roy', 0);
