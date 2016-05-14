151002bb90d1d82964981735a7146ec3c86ea9a2
Roy Falik <roy.falik@utexas.edu>
2016-05-14 17:31:34 -0500

    updated schema

--- default_inserts.sql
+++ default_inserts.sql
@@ -1,13 +1,15 @@
- ALTER TABLE challenge_table ALTER issued SET DEFAULT CURRENT_TIMESTAMP;
- ALTER TABLE duel_table ALTER time_started SET DEFAULT CURRENT_TIMESTAMP;
- ALTER TABLE duel_table ALTER health_sender SET DEFAULT 100;
- ALTER TABLE duel_table ALTER health_recipient SET DEFAULT 100;
- ALTER TABLE duel_table ALTER moves_in_duel SET DEFAULT 0;
- ALTER TABLE duel_table ALTER winner_id SET DEFAULT 'none';
-
-
  insert into user_table(id, name, in_duel) VALUES ('10206557582650156', 'jeff', 0);
  insert into user_table(id, name, in_duel) VALUES ('10205320360242528', 'roy', 0);
+
+ 	--in relational model:
+ 	--unique constraint on challenge_table sender/reciever
+ 	--unique constraint on user_table name
+ 	--default values: wins/losses/draws/games = 0 in user_table
+ 	--default values: health = 100 in duel_table
+ 	--default values: winner_id = 'none' in duel_table
+ 	--default values: moves = 0 in duel_table
+ 	--default values: issued/time_started = CURRENT_TIMESTAMP in challenge_table/duel_table
+
 
  	--in schema.ddl:
  	--change VARCHAR2 to VARCHAR
