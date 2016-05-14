4057b408f2932910941ef425122f90990f8d4708
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-14 18:20:47 -0500

    testing help

--- index.js
+++ index.js
@@ -165,36 +165,40 @@ function sendTextMessage(sender, text) {
     });
 }
 
+
+
+
 function sendGenericMessage(sender) {
     messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"receipt",
+            // "recipient_name":"Stephane Crozatier",
+            // "order_number":"12345678902",
+            // "currency":"USD",
+            // "payment_method":"Visa 2345",        
+            // "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+            // "timestamp":"1428444852", 
+            "elements":[
+              {
+                "title":"Classic White T-Shirt",
+                "subtitle":"100% Soft and Luxurious Cotton",
+                // "quantity":2,
+                // "price":50,
+                // "currency":"USD",
+                // "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+              },
+              {
+                "title":"Classic Gray T-Shirt",
+                "subtitle":"100% Soft and Luxurious Cotton",
+                // "quantity":1,
+                // "price":25,
+                // "currency":"USD",
+                // "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+              }
+            ]
+          }
         }
     };
     request({
