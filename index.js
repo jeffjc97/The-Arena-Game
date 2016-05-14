6fc59f3eefe62eebe6e98685fee433c3463186dc
Roy Falik <roy.falik@utexas.edu>
2016-05-14 15:02:53 -0500

    debug

--- index.js
+++ index.js
@@ -460,7 +460,7 @@ function makeMoveSetup(su, s){
         client.query(q, function(err, result) {
             done();
             if (err || result.rows.length !== 1) {
-                sendTextMessage(s, json.stringify(err).substring(0,200));
+                sendTextMessage(s, JSON.stringify(err).substring(0,200));
             }
             else {
                 duel_id = result.rows[0].in_duel;
