61df25891a42d7bebbec610658582d9dae5666d9
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-14 19:23:32 -0500

    help

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ function sendGenericMessage(sender) {
               {
                 "title":"Help",
                 "subtitle":"100% Soft and Luxurious Cotton\nlolol\nlolol",
-                "image_url":"http://i.imgur.com/ghPdYrT.png"
+                "image_url":"http://i.imgur.com/I9quwyf.png"
               },
             ]
           }
