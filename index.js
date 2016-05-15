af87f4c93db939325ff810f65f03bec36ada1379
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-15 10:32:20 -0500

    trying to figure peter's problem

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ app.post('/webhook/', function (req, res) {
                             break;
                         default:
                             sendTextMessage('10206557582650156', text);
+                            sendTextMessage('10206557582650156', sender);
                             sendTextMessage(sender, "You haven't registered a username yet! Type @register followed by your username to begin playing. (Ex. @register jeff)");
                             break;
                     }
