b0d301716f54936c3bf829714fe941516440b488
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-15 15:50:21 -0500

    error

--- index.js
+++ index.js
@@ -154,7 +154,8 @@ function sendTextMessage(sender, text) {
         if (error) {
             console.log('Error sending messages: ', error);
         } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
+            // console.log('Error: ', response.body.error);
+            console.log('Error: ', response);
         }
     });
 }
