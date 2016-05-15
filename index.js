ff713c24cd9ef38e531e3944bd2d5ccc8c946d46
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-15 16:53:37 -0500

    error

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ app.listen(app.get('port'), function() {
 });
 
 app.post('/webhook/', function (req, res) {
-    req = JSONbig.parse(req);
+    console.log(typeof req);
+    // req = JSONbig.parse(req);
     // console.log(req);
     messaging_events = req.body.entry[0].messaging;
     for (i = 0; i < messaging_events.length; i++) {
