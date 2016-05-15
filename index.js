b3d150b8ad0ea61f42907467d7d6f50063b99679
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-14 21:12:50 -0500

    forfeit

--- index.js
+++ index.js
@@ -593,8 +593,23 @@ function sendError(uid, eid, msg) {
     }
 }
 
-function forfeitDuel(uid) {
-    return;
+function forfeitDuel(lid) {
+    q_get_did = "SELECT in_duel FROM user_table where id = \'" + lid + "\'";
+    e = function(err) {
+        sendError(lid, 37);
+    };
+    s_get_all_info = function(result) {
+        sendTextMessage(lid, "woo");
+        sendTextMessage(lid, JSON.stringify(result.rows).substring(0,300));
+    };
+    s_get_did = function(result) {
+        data =result.rows[0];
+        if (data.in_duel) {
+            q_get_all_info = "SELECT u.name, u.id FROM duel_table d INNER JOIN user_table u ON (u.id = d.recipient OR u.id = d.sender) WHERE d.duel_id = " + data.in_duel;
+            makeQuery(q_get_all_info, e, s_get_all_info);
+        }
+    };
+    makeQuery(q_get_did, e, s_get_did);
 }
 
 function loseDuel(lid, wid, lname, wname, did) {
