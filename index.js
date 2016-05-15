c53b23df6d9a4db2443250e9e2224160ac3409d5
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-15 17:10:32 -0500

    error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var request = require('request');
 var pg = require('pg');
 var JSONbig = require('json-bigint');
