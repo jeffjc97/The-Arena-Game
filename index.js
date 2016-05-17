a523011729b48d9df9a5c987e36db00b8343006a
jeffjc97 <jeffreychang@college.harvard.edu>
2016-05-17 11:34:43 -0500

    minor change to loseduel grammer

--- index.js
+++ index.js
@@ -686,8 +686,8 @@ function loseDuel(lid, wid, lname, wname, did) {
             sendError(wid, 27);
         }
         s_update_l = function(result) {
-            sendTextMessage(lid, "You were defeated by " + wname + ".");
-            sendTextMessage(wid, "You have defeated " + lname + "!");
+            sendTextMessage(lid, "You were defeated by " + wname + ". Duel ending in 5 seconds...");
+            sendTextMessage(wid, "You have defeated " + lname + "! Duel ending in 5 seconds...");
         }
         s_update_w = function(result) {
             makeQuery(q_update_l, e, s_update_l);
