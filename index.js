7338afdd196ab321c8bf544d32bcdad65ddfb066
Roy Falik <roy.falik@utexas.edu>
2016-05-20 16:35:36 -0500

    stat changes

--- index.js
+++ index.js
@@ -383,7 +383,6 @@ function getPersonalInfo(s){
             username = result.rows[0].name;
             getPendingChallenges(s);
             getStats(username, s);
-            sendTextMessage(s, "You have "+ result.rows[0].points+" coins.");
         }
     };
     makeQuery(q_get_username, e, s_get_username);
@@ -850,6 +849,8 @@ function getStats(user, s){
                 pct = "N/A";
             }
             sendTextMessage(s, "STATS: " + user + "\nWins: " + data.wins + "\nLosses: " + data.losses+"\nDraws: " + data.draws + "\nGames: " + data.games_played + "\nWin %: " + pct);
+            sendTextMessage(s, user + " has "+ data.points+" coins.");
+
         }
         else{
             sendTextMessage(s, "User not found.");
