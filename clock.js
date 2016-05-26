97e1bc5f5692cc393a9984f51a1ecb03b087b199
Roy Falik <roy.falik@utexas.edu>
2016-05-25 22:09:18 -0500

    updating timers

--- clock.js
+++ clock.js
@@ -1,20 +1,9 @@
-var express = require('express');
-// var bodyParser = require('body-parser');
-var request = require('request');
-var pg = require('pg');
-var JSONbig = require('json-bigint');
-var app = express();
-var token = "EAADO0pQrRbsBAD8aZB2wCeI1zwFlCVS9W1HGQJQVSQj3Qk837u5agR0Gphg7zaZBOyhkVrRVloP2uZAsNXcZCqDXqc49aP26h1IgZBZCTAEhkIiksjxtx2j895suRIbZBGZB3tZChW4J0lNdNMc8jGGNWSayIR8RQru1CnP9sk3ZCC0gZDZD";
-pg.defaults.ssl = true;
-
-
-
 //OnInterval
 var threeSecondInterval = function(){
     sendTextMessage(10205320360242528, "Another 3 seconds have gone by. What did you do in them?");
     sendTextMessage(10206557582650156, "Another 3 seconds have gone by. What did you do in them?");
 }
-setInterval(threeSecondInterval, 3000)
+setInterval(threeSecondInterval, 3000);
 
 //For specific times, use a chron job
 var fifteenSeconsAfterMinute = function() {
@@ -28,27 +17,3 @@ new CronJob({
   start: true,
   timeZone: "America/Los_Angeles"
 });
-
-
-
-function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    };
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-            // console.log('Error: ', response);
-        }
-    });
-}
