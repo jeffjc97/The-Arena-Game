88667dc908004a30a0d4060486fe995689d42d26
jeffjc97 <jeffreychang@college.harvard.edu>
2016-06-18 00:15:35 -0700

    trying to get back on track

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging;
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i];
-        sender = event.sender.id.toString();
+        sender = event.sender.id;
         if (event.message && event.message.text) {
             text = event.message.text;
             words = text.split(" ");
