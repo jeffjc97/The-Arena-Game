18a1ec068fdeb41a25ab913463ef06366aeb8d20
Roy Falik <roy.falik@utexas.edu>
2016-06-25 13:52:02 -0500

    schema changes

--- schema_func.sql
+++ schema_func.sql
@@ -9,3 +9,21 @@ $new_user$ LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS new_user ON user_table;
 CREATE TRIGGER new_user AFTER INSERT ON user_table
      FOR EACH ROW EXECUTE PROCEDURE new_user_trigger();
+
+CREATE OR REPLACE FUNCTION feedback_trigger() RETURNS trigger AS $feedback$
+DECLARE
+	count_var INTEGER;
+BEGIN
+	SELECT count(id) into count_var FROM feedback_table WHERE id=NEW.id AND TIME > (CURRENT_TIMESTAMP - INTERVAL '10 seconds');
+    IF count_var > 0 THEN
+    	RETURN null;
+    ELSE
+    	RETURN NEW;
+    END IF;
+END;
+$feedback$ LANGUAGE plpgsql;
+
+
+DROP TRIGGER IF EXISTS feedback ON feedback_table;
+CREATE TRIGGER feedback BEFORE INSERT ON feedback_table
+     FOR EACH ROW EXECUTE PROCEDURE feedback_trigger();
